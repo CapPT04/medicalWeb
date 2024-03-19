92cccda9421dcc8247a30ce60d5c9ddc6b2b86e5
Cap PT <phatasdasd0198@gmail.com>
2024-03-19 16:26:37 +0700

    Update And Delte In CRUD

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -37,10 +37,52 @@ let displayGetCRUD = async (req, res) => {
         dataTable: data
     })
 }
+
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    console.log(userId);
+    if (userId) {
+        let userData = await CRUDService.getUserInforById(userId);
+
+
+        return res.render('editCRUD.ejs', {
+            user: userData
+        })
+
+    } else {
+        return res.send("User not found!");
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    })
+
+
+}
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('Delete user succeed!')
+    } else {
+        return res.send('User not found!')
+    }
+
+}
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -1,8 +1,8 @@
 import bcrypt from 'bcryptjs'
 import db from '../models/index'
+import user from '../models/user';
 
 const salt = bcrypt.genSaltSync(10);
-
 
 let createNewUser = async (data) => {
     return new Promise(async (resolve, reject) => {
@@ -12,8 +12,8 @@ let createNewUser = async (data) => {
                 email: data.email,
                 passwork: hashPassworkFromBcrypt,
                 firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
+                lastName: data.lastName, // Sửa thành lastName
+                address: data.address, // Sửa thành address
                 phoneNumber: data.phoneNumber,
                 gender: data.gender === '1' ? true : false,
                 roleId: data.roleId,
@@ -24,7 +24,6 @@ let createNewUser = async (data) => {
             reject(e);
         }
     })
-
 }
 
 let hashUserPasswork = (passwork) => {
@@ -51,7 +50,68 @@ let getAllUser = () => {
     })
 }
 
+let getUserInforById = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userId },
+                raw: true,
+            })
+            if (user) {
+                resolve(user)
+
+            } else {
+                resolve({})
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+let updateUserData = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: data.id }
+            })
+            if (user) {
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+                user.address = data.address;
+                await user.save();
+
+                let allUsers = await db.User.findAll();
+                resolve(allUsers);
+            } else {
+                resolve();
+            }
+        } catch (e) {
+            console.log(e);
+
+        }
+    })
+}
+
+let deleteUserById = (userID) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userID }
+            })
+            if (user) {
+                await user.destroy();
+            }
+
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
 module.exports = {
     createNewUser: createNewUser,
     getAllUser: getAllUser,
+    getUserInforById: getUserInforById,
+    updateUserData: updateUserData,
+    deleteUserById: deleteUserById,
 }
